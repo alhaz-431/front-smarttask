3a322a27d2cba4caf33ffddc086f8bce3d625960
Alhaz <alhaz@gmail.com>
2026-06-08 16:01:19 +0600

    updated page

--- src/app/(dashboard)/projects/page.tsx
+++ src/app/(dashboard)/projects/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
 
-// পাথটি নিশ্চিত করুন: components/auth/ProjectsContent
 const ProjectsContent = dynamic(() => import("@/components/auth/ProjectsContent"), {
   ssr: false, 
 });
